5791ccc8f2adb73ae8a9ff9476a71a76f11fc084
jperaza1 <jperaza@oktopus-hn.com>
2017-08-04 18:14:29 -0600

    se agrego cemantic y se creo el primer componente y se agrego a app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { VotesApp } from './votes/votes.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VotesApp
   ],
   imports: [
     BrowserModule
---
+++ src/app/votes/votes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'votes-app',
+    templateUrl: './votes.component.html',
+    styleUrls: ['./votes.component.css']
+})
+export class VotesApp{
+    title: string = "Votes App";
+}
